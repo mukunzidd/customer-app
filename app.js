a41a24b6c225c83d5757990a871dd00eba948452
Dieudonne Mukunzi <dmukunzi0@gmail.com>
2017-01-22 14:33:22 +0200

    Installed Express Validator module and implemented it to my add user form .EJS file. I guess the last step is to implement Mongodb and have it save my users instead of returning everythin into the console.

--- app.js
+++ app.js
@@ -1,11 +1,10 @@
 // This is the entry file
 // Requiring node modules
-var express = require('express');
-var bodyParser = require('body-parser');
-var path = require('path');
-
-// Initialise a var for the entry file
-var app = express();
+var express = require('express'),
+    bodyParser = require('body-parser'),
+    expressValidator = require('express-validator'),
+    path = require('path'),
+    app = express();
 
 // View Engine
 app.set('view engine', 'ejs');
@@ -15,24 +14,44 @@ app.set('views', path.join(__dirname, 'views'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 
-// Set statis path
+// Set static path
 app.use(express.static(path.join(__dirname, 'public')));
+
+// Global Vars
+
+// Middleware for expressValidator
+app.use(expressValidator({
+  errorFormatter: function(param, msg, value) {
+      var namespace = param.split('.')
+      , root    = namespace.shift()
+      , formParam = root;
+
+    while(namespace.length) {
+      formParam += '[' + namespace.shift() + ']';
+    }
+    return {
+      param : formParam,
+      msg   : msg,
+      value : value
+    };
+  }
+}));
 
 // Objects to pass in views
 var users = [
     {
-        name:'Jane',
-        lastname:'Doe',
+        first_name:'Jane',
+        last_name:'Doe',
         email:'janedoe@gmail.com'
     },
     {
-        name:'John',
-        lastname:'Doe',
+        first_name:'John',
+        last_name:'Doe',
         email:'johndoe@gmail.com'
     },
     {
-        name:'Jill',
-        lastname:'Doe',
+        last_name:'Jill',
+        last_name:'Doe',
         email:'jilldoe@gmail.com'
     }
 ]
@@ -44,6 +63,30 @@ app.get('/', function(req, res){
     });
 });
 
+app.post('/users/add', function(req, res){
+
+  req.checkBody('first_name', 'Frist Name is required').notEmpty();
+  req.checkBody('last_name', 'Last Name is required').notEmpty();
+  req.checkBody('email', 'Email is required').notEmpty();
+
+  var errors = req.validationErrors();
+
+  if (errors){
+      res.render('index', {
+        title:'Users',
+        users: users,
+        errors: errors
+    });
+  } else {
+      var newUser = {
+        first_name: req.body.first_name,
+        last_name: req.body.first_name,
+        email: req.body.first_name
+    }
+
+    console.log('SUCCESS');
+  }
+});
 // Create a server
 app.listen(3000, function(){
     console.log('Server Started on Port 3000...')
